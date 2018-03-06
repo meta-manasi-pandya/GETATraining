e2115e78967642ec83aa3addcb728e5bd8aa90da
Manasi Pandya <manasi.pandya@metacube.com>
2018-03-06 15:00:57 +0530

    Updated Design Pattern Session-1

---
+++ src/main/designpattern/designPattern1/com/metacube/shoppingcart/dao/DatabaseEnum.java
@@ -0,0 +1,10 @@
+package main.designpattern.designPattern1.com.metacube.shoppingcart.dao;
+
+/**
+ * this enum contains storage type
+ * @author - Manasi Pandya 
+ *
+ */
+public enum DatabaseEnum {
+	IN_MEMORY , SQL;
+}
---
+++ src/main/designpattern/designPattern1/com/metacube/shoppingcart/dao/StatusEnum.java
@@ -0,0 +1,13 @@
+package main.designpattern.designPattern1.com.metacube.shoppingcart.dao;
+
+/**
+ * this enum contains status of operation over product
+ * @author- Manasi Pandya  
+ *
+ */
+public enum StatusEnum {
+	PRODUCT_ADDED_SUCCESSFULLY,
+	PRODUCT_UPDATED_SUCCESSFULLY,
+	PRODUCT_REMOVED_SUCCESSFULLY,
+	PRODUCT_NOT_FOUND;
+}
